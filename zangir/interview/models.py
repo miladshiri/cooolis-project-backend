5d3b2cd0eabc96be482cbd4077ea9d75a599cfc2
Milad Shiri <miladonlinemail@gmail.com>
2020-02-22 20:58:49 +0330

    define Question model

--- zangir/interview/models.py
+++ zangir/interview/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class ZangirBaseModel(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now_add=False)
+    
+    class Meta:
+        abstract = True
+
+class Question(ZangirBaseModel):
+    ONE_ANSWER = 'OA'
+    MULTIPLE_ANSWER = 'MA'
+
+    QUESTION_TYPES = [
+        (ONE_ANSWER, 'one_answer'),
+        (MULTIPLE_ANSWER, 'multiple_answer')
+    ]
+
+    question = models.TextField(max_length=200)
+    qtype = models.TextField(choices=QUESTION_TYPES)
+    
+
