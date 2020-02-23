c263fad3360a810a3c9f71f9a32c5ec58209869a
Milad Shiri <miladonlinemail@gmail.com>
2020-02-23 11:40:27 +0330

    initialize restframework

---
+++ zangir/interview/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Question
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['question', 'qtype']
+
+        
--- zangir/zangir/settings.py
+++ zangir/zangir/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'interview'
+    'rest_framework',
+    'interview',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+#Django Rest Framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
