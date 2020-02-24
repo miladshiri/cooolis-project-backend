f4847e74e3c31d9b1816d160f68aed5b754f5a57
Milad Shiri <miladonlinemail@gmail.com>
2020-02-24 11:54:54 +0330

    New migrations - Change on models attributes and fields - add Option model

--- zangir/interview/models.py
+++ zangir/interview/models.py
@@ -8,18 +8,46 @@ class ZangirBaseModel(models.Model):
     class Meta:
         abstract = True
 
+
 class Question(ZangirBaseModel):
-    ONE_ANSWER = 'OA'
-    MULTIPLE_ANSWER = 'MA'
+    UNKNOWN_TYPE = 'UT'
+    ONE_ANSWER_TYPE = 'OA'
+    MULTIPLE_ANSWER_TYPE = 'MA'
+    EMPTY_SPACE_TYPE = 'ES'
 
-    QUESTION_TYPES = [
-        (ONE_ANSWER, 'one_answer'),
-        (MULTIPLE_ANSWER, 'multiple_answer')
-    ]
-
-    question = models.TextField(max_length=300)
-    qtype = models.TextField(choices=QUESTION_TYPES)
+    text = models.TextField(max_length=300)
+    complexity = models.IntegerField(default=2)
 
 
     def __str__(self):
-        return self.question[:20]
+        return self.text[:20]
+
+
+    def options(self):
+        return self.option_set.all()
+
+    def answers(self):
+        return self.options().filter(is_answer=True)
+
+    def question_type(self):
+        answer_num = self.answers().count()
+        options_num = self.options().count()
+
+        if answer_num == 1 and options_num == 1:
+            return self.EMPTY_SPACE_TYPE
+        elif answer_num == 1 and options_num > 1:
+            return self.ONE_ANSWER_TYPE
+        elif answer_num > 1:
+            return self.MULTIPLE_ANSWER_TYPE
+        else:
+            return self.UNKNOWN_TYPE
+
+
+class Option(ZangirBaseModel):
+    text = models.TextField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_answer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text[:20]
+
